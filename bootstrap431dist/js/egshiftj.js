79a476e22f6d4ef074df88e16926d56681508b5f
Khushiram malik <apmalik33@gmail.com>
2019-10-04 08:21:01 +0530

    uploads
    this is extra

---
+++ bootstrap431dist/js/egshiftj.js
@@ -0,0 +1,12 @@
+$("#bslider").vegas({
+    slides: [
+        { src: "./bootstrap431dist/img/packers-movers-gurugram.jpg" }
+        
+       
+    ],
+    overlay: true
+});
+
+$(function () {
+    $('#main_navbar').bootnavbar();
+  });
